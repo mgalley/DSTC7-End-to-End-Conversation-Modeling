f011eb67d70334824d1aac7a442c0bc9b78dc32f
U-REDMOND\mgalley <mgalley@MGALLEY2.redmond.corp.microsoft.com>
2018-06-18 08:42:48 -0700

    fix log file names

--- data_extraction/src/create_trial_data.sh
+++ data_extraction/src/create_trial_data.sh
@@ -10,8 +10,8 @@ TRIAL=`cat lists/data-trial.txt`
 
 for id in $TRIAL; do
 	echo Downloading $id
-	wget https://files.pushshift.io/reddit/submissions/RS_$id.bz2 -O reddit/RS_$id.bz2 -o reddit/RS_$id.bz2.log -c
-	wget https://files.pushshift.io/reddit/comments/RC_$id.bz2 -O reddit/RC_$id.bz2 -o reddit/RC_$id.bz2.log -c
+	wget https://files.pushshift.io/reddit/submissions/RS_$id.bz2 -O reddit/RS_$id.bz2 -o logs/RS_$id.bz2.log -c
+	wget https://files.pushshift.io/reddit/comments/RC_$id.bz2 -O reddit/RC_$id.bz2 -o logs/RC_$id.bz2.log -c
 	python src/create_trial_data.py --rsinput=reddit/RS_$id.bz2 --rcinput=reddit/RC_$id.bz2 --subreddit_filter=lists/subreddits.txt --domain_filter=lists/domains.txt --pickle=data/$id.pkl --facts=data/$id.facts.txt --convos=data/$id.convos.txt > logs/$id.log 2> logs/$id.err
 done
 
