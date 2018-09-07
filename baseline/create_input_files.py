ab800fda48ec80e0f362bc01833d30459c313e58
DSTC-MSR-NLP <39747567+DSTC-MSR-NLP@users.noreply.github.com>
2018-09-07 11:55:57 -0700

    Update create_input_files.py

--- baseline/create_input_files.py
+++ baseline/create_input_files.py
@@ -1,9 +1,10 @@
 import os, queue
-from baseline import SOS_token, EOS_token
+from baseline import SOS_token, EOS_token, UNK_token
+
 
 
 def main(path_txt, fld_out,
-	delimiter='\t'
+	max_vocab_size=2e4,
 	):
 
 
@@ -23,12 +24,18 @@ def main(path_txt, fld_out,
 
 	sources = []
 	targets = []
+	n = 0
 	for line in lines:
-		sub =  line.split(delimiter)
+		n += 1
+		if n%1e5 == 0:
+			print('checked %.2fM/%.2fM lines'%(n/1e6, len(lines)/1e6))
+		sub =  line.split('\t')
 		source = sub[-2]
 		target = sub[-1]
-		sources.append(source.strip().split(' '))
-		targets.append(target.strip().split(' '))
+		if source == 'START':	# skip if source has nothing
+			continue
+		sources.append(source.strip().split())
+		targets.append(target.strip().split())
 
 	vocab = dict()
 	for tokens in sources + targets:
@@ -41,36 +48,53 @@ def main(path_txt, fld_out,
 	for token in vocab:
 		pq.put((-vocab[token], token))
 
-	ordered_tokens = [SOS_token, EOS_token]
+	ordered_tokens = [SOS_token, EOS_token, UNK_token]
 	while not pq.empty():
 		freq, token = pq.get()
 		ordered_tokens.append(token)
+		if len(ordered_tokens) == max_vocab_size:
+			break
+
+	print('vocab size = %i'%len(ordered_tokens))
 
 	token2index = dict()
 	for i, token in enumerate(ordered_tokens):
-		token2index[token] = str(i + 1)
+		token2index[token] = str(i + 1)			# +1 as 0 is for padding
 	with open(path['dict'], 'a', encoding="utf-8") as f:
 		f.write('\n'.join(ordered_tokens))
 
 	nums = []
 	for tokens in sources:
-		nums.append(' '.join([token2index[token] for token in tokens]))
+		nums.append(' '.join([token2index.get(token, token2index[UNK_token]) for token in tokens]))
 	with open(path['source'], 'a', encoding="utf-8") as f:
 		f.write('\n'.join(nums))
 
 	nums = []
 	for tokens in targets:
-		nums.append(' '.join([token2index[token] for token in tokens]))
+		nums.append(' '.join([token2index.get(token, token2index[UNK_token]) for token in tokens]))
 	with open(path['target'], 'a', encoding="utf-8") as f:
 		f.write('\n'.join(nums))
 
 
+def extract_head(path, n=30):
+	lines = []
+	for line in open(path, encoding='utf-8'):
+		lines.append(line)
+		if len(lines) == n:
+			break
+	with open(path+'.head', 'w', encoding='utf-8') as f:
+		f.write(''.join(lines))
+
 
 
 if __name__ == '__main__':
-	fld_out = 'trial'
-	path_txt = os.path.join('trial','trial.convos.txt')
+	#fld_out = 'trial'
+	#path_txt = os.path.join('trial','trial.convos.txt')
+	fld_out = 'official'
+	path_txt = 'F:/DSTC/data-official/train.convos.txt'
+	#extract_head(path_txt)
 	main(path_txt, fld_out)
+	#build_vocab(path_txt, fld_out)
 	print('done!')
 
 
