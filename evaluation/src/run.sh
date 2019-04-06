e3c2adf642488714ccc8e8c375e633c059e44255
U-REDMOND\mgalley <mgalley@mgalley2.redmond.corp.microsoft.com>
2019-04-05 17:15:26 -0700

    saved from crashed hard drive

---
+++ evaluation/src/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#python dstc.py -c all --refs ../../data_extraction/test.refs  >& log
+python dstc.py -r 1 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 2 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 3 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 4 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 5 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 6 -c all --refs ../../data_extraction/test.refs  >& log
+#python dstc.py -r 0 -c all --refs ../../data_extraction/test.refs  >& log
